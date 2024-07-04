2a477d78aa802d23c24fe957969c2dffb01715a0
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-07-04 08:34:28 +0100

    second variants

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,15 @@
 import { SideNavigation } from "@/components/navigation/sidebar";
 import { Navbar, Footer } from "@/components/navigation";
+import { Suspense } from "react";
+import { SkeletonNavbar } from "@/components/skelton";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SideNavigation />
-      <Navbar />
+      <Suspense fallback={<SkeletonNavbar />}>
+        <Navbar />
+      </Suspense>
       <section className="w-full relative  md:pl-[96px] min-[1140px]:pl-[270px] ">
         <div className="flex w-full flex-col h-full relative max-container">
           {children}
